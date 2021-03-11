c8af365c588edfd171edbc1d7f3255984e8ec2c7
ValeGoyeneche <m.goyenechea@uniandes.edu.co>
2021-03-10 22:27:13 -0500

    Entrega final Reto1

--- App/model.py
+++ App/model.py
@@ -1,71 +1,76 @@
-﻿"""
- * Copyright 2020, Departamento de sistemas y Computación,
- * Universidad de Los Andes
- *
- *
- * Desarrolado para el curso ISIS1225 - Estructuras de Datos y Algoritmos
- *
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along withthis program.  If not, see <http://www.gnu.org/licenses/>.
- *
- * Contribuciones:
- *
- * Dario Correal - Version inicial
- """
-
-
 import config as cf
 from DISClib.ADT import list as lt
+from DISClib.ADT import map as mp
+from DISClib.DataStructures import mapentry as me
 from DISClib.Algorithms.Sorting import shellsort as sa
-from DISClib.Algorithms.Sorting import selectionsort as ss
-from DISClib.Algorithms.Sorting import insertionsort as si
-from DISClib.Algorithms.Sorting import mergesort as mer
-from DISClib.Algorithms.Sorting import quicksort as qu
-assert cf
+import datetime
 
-"""
-Se define la estructura de un catálogo de videos. El catálogo tendrá dos listas, una para los videos, otra para las categorias de
-los mismos.
-"""
-def initcatalog(listType):
-    return {"videos":lt.newList(listType)}
 
-def subList(list, size):
-    muestra=lt.subList(list,0,size)
-    return {"lista":muestra}
-    
-# Construccion de modelos
+def newCatalog():
+    catalog = {'videos': None, 'categories': None}
+    catalog['videos'] = lt.newList()
+    catalog['categories'] = lt.newList()
+    return catalog
 
-# Funciones para agregar informacion al catalogo
-def addvideo(catalog,video):
-    lt.addLast(catalog["videos"],video)
-# Funciones para creacion de datos
 
-# Funciones de consulta
+def emptyList():
+    return lt.newList('SINGLE_LINKED')
 
-# Funciones utilizadas para comparar elementos dentro de una lista
-def cmpVideosByViews(video1, video2):
-    return (int(video1['views']) < int(video2['views']))
-# Funciones de ordenamiento
-def sortVideos(catalog, listSortType):
-    if listSortType==1:
-        ss.sort(catalog, cmpVideosByViews)
-    elif listSortType==2:
-        si.sort(catalog, cmpVideosByViews)
-    elif listSortType==3:
-        sa.sort(catalog, cmpVideosByViews)
-    elif listSortType==4:
-        mer.sort(catalog, cmpVideosByViews)
-    else:
-        qu.sort(catalog, cmpVideosByViews)
+
+def addVideo(catalog, video):
+    lt.addLast(catalog["videos"], video)
+
+
+def addCategory(catalog, category):
+    lt.addLast(catalog["categories"], category)
+
+
+def getFirstVideo(catalog):
+    return lt.firstElement(catalog["videos"])
+
+
+def getVideosByCountry(catalog, country):
+    countryList = mp.get(catalog['videos'], country)
+    if countryList:
+        return me.getValue(countryList)
+    return None
+
+
+def getTrendingDays(video):
+    publishTimeString = video['publish_time'].split("-")
+    publishTime = datetime.datetime(int(
+        publishTimeString[0]), int(publishTimeString[1]), int(publishTimeString[2][0:2]))
+    trendingDateString = video['trending_date'].split(".")
+    trendingDate = datetime.datetime(int(
+        "20"+trendingDateString[0]), int(trendingDateString[2]), int(trendingDateString[1]))
+    return (trendingDate - publishTime).days
+
+
+def compareViews(video1, video2):
+    return (int(video1['views']) > int(video2['views']))
+
+
+def compareTrendingDays(video1, video2):
+    return (getTrendingDays(video1) > getTrendingDays(video2))
+
+
+def compareLikes(video1, video2):
+    return (int(video1['likes']) > int(video2['likes']))
+
+
+def sortVideosByViews(catalog):
+    lst = catalog['videos']
+    sa.sort(lst, compareViews)
+    return lst
+
+
+def sortVideosByTrendingDays(catalog):
+    lst = catalog['videos']
+    sa.sort(lst, compareTrendingDays)
+    return lst
+
+
+def sortVideosByLikes(catalog):
+    lst = catalog['videos']
+    sa.sort(lst, compareLikes)
+    return lst
--- README.md
+++
@@ -1,2 +0,0 @@
-#Req. 2 - Sebastián Contreras Salazar, 202020903, s.contrerass@uniandes.edu.co
-#Req. 3 - Valentina Goyeneche, 201922380, m.goyenechea@uniandes.edu.co
